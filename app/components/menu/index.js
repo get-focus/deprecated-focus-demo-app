b8edc58ce15512c164136c05b8708cad0ec3df4c
TomGallon <thomas.gallon@gmail.com>
2016-10-31 15:27:33 +0100

    fix menu

---
+++ app/components/menu/index.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+//import {navigate} from 'focus-core/history';
+import Menu from 'focus-components/menu';
+import {component as Modal} from 'focus-components/modal';
+//import {quickSearchStore} from 'focus-core/search/built-in-store';
+//import dispatcher from 'focus-core/dispatcher';cd
+//
+
+
+//custom web component
+//import QuickSearchView from '../search/quick';
+
+const menus = [
+    { icon: 'home', label: 'menu.home', route: '/' }, // route: 'home'
+    { icon: 'search', label: 'menu.search', handleOnClick: () => { /*this._onQuickSearchModalToggle()*/ }},
+    { icon: 'settings', label: 'menu.admin', route: '/admin/masterdata'}
+];
+
+class MenuDemo extends Component {
+    render() {
+        return (
+            <Menu menus={menus} />
+        )
+    }
+}
+MenuDemo.displayName = 'MenuDemo';
+export default MenuDemo;
