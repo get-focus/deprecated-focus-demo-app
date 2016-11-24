c0021a23c46e5893ec3937747dd53933b58fde10
Thomas <tgallon@gmail.com>
2016-11-24 23:44:07 +0100

    add global exemple prop

--- app/config/search/movie-metadatas.js
+++ app/config/search/movie-metadatas.js
@@ -1,14 +1,16 @@
-import React, {PropTypes} from 'react';
+import React, {PropTypes, PureComponent} from 'react';
 import {compose} from 'redux';
 import {connect as connectToState} from 'react-redux';
 import {connect as connectToMetadata} from 'focus-graph/behaviours/metadata';
 import {connect as connectToFieldHelpers} from 'focus-graph/behaviours/field';
 import {buildFieldForLineSearch} from 'focus-search/store';
 
+import Button from 'focus-components/button';
 
-function PureMovieLine({textFor, movId, ...props}) {
+function PureMovieLine({textFor, ...props}) {
     return (
-        <div key={movId} data-demo='movie-line'>
+        <div key={props.movId} data-demo='movie-line'>
+            <div className='level1'>{props.movId}</div>
             <div className='level1'>{textFor('title', {entityPath: 'movie'})}</div>
             <div className='level2'>{textFor('movieType', {entityPath: 'movie'})}</div>
             <div className='level3'>{textFor('productionYear', {entityPath: 'movie'})}</div>
@@ -27,10 +29,26 @@ const MovieLine = compose(
     connectToFieldHelpers()
 )(PureMovieLine);
 
+class GlobalGroupActions extends PureComponent {
+    constructor(props) {
+        super(props);
+        this._onClickAction = this._onClickAction.bind(this);
+    }
+    _onClickAction(evt) {
+        console.log('actions globales', this.props);
+    }
+    render() {
+        return (
+            <Button label="Actions globales Movies" onClick={this._onClickAction} />
+        );
+    }
+};
 
 export default {
     lineIdentifierProperty: 'movId',
     LineComponent: props => (<MovieLine {...props} />),
+    GlobalGroupActionsComponent: props => (<GlobalGroupActions {...props} />),
+    //ActionsComponent: props => (<Button label={`click sur ${props.movId}`} />),
     actionsLine: [
         {label: 'Yo', icon: 'print', action: () => {console.log('action')}},
         {label: 'La', icon: 'print', action: () => {console.log('action')}}
