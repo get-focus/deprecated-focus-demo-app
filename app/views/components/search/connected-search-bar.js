69356679aa52b5beaae0a18136371d9d5cd8a963
Thomas <tgallon@gmail.com>
2016-12-13 19:34:46 +0100

    fix-search-redirect

--- app/views/components/search/connected-search-bar.js
+++ app/views/components/search/connected-search-bar.js
@@ -1,4 +1,4 @@
-import React, {PropTypes, PureComponent} from 'react';
+import React, {PureComponent} from 'react';
 import compose from 'lodash/flowRight';
 import {connect as connectToSearch} from 'focus-search/behaviours/search';
 import SearchBar from 'focus-search/components/searchbar';
@@ -12,22 +12,12 @@ const searchOptions = {
 // search bar component
 class SearchBarComponent extends PureComponent {
     render() {
-        const {onChange, SearchBarProps: {scope, scopes, term, unitSearchDispatch: {queryAction, scopeAction}}} = this.props;
+        const {SearchBarProps: {scope, scopes, term, unitSearchDispatch: {queryAction, scopeAction}}} = this.props;
         return (
-            <SearchBar
-                onChange={onChange}
-                queryAction={queryAction}
-                scope={scope}
-                scopeAction={scopeAction}
-                scopes={scopes}
-                term={term} />
+            <SearchBar queryAction={queryAction} scopes={scopes} scope={scope} scopeAction={scopeAction} term={term} />
         );
     };
 };
-SearchBarComponent.displayName = 'SearchBarComponent';
-SearchBarComponent.propTypes = {
-    onChange: PropTypes.func
-}
 
 // search bar component connected to search store
 const ConnectedSearchBarComponent = compose(
--- app/views/components/search/header.js
+++ app/views/components/search/header.js
@@ -1,31 +1,12 @@
-import React, {PropTypes, PureComponent} from 'react';
+import React from 'react';
 import i18next from 'i18next';
 import ConnectedSearchBarComponent from './connected-search-bar';
-import {withRouter} from 'react-router';
 
-export class SearchHeaderWithRouter extends PureComponent {
-    render() {
-        const {router} = this.props;
-        return <SearchHeader onChange={() => router.push('/search/advanced')} />
-    }
-}
-SearchHeaderWithRouter.displayName = 'SearchHeaderWithRedirect';
-export const SearchHeaderWithRedirect = withRouter(SearchHeaderWithRouter);
-
-
-class SearchHeader extends PureComponent {
-    render() {
-        const {onChange} = this.props;
-        return (
-            <div data-demo='search-header'>
-                <h1>{i18next.t('search.cartridge.title')}</h1>
-                <ConnectedSearchBarComponent onChange={onChange} />
-            </div>
-        );
-    }
-};
-SearchHeader.displayName = 'SearchHeader';
-SearchHeader.propTypes = {
-    onChange: PropTypes.func
-}
-export default SearchHeader;
+const HomeHeader = () => (
+    <div data-demo='home-header'>
+        <h1>{i18next.t('search.cartridge.title')}</h1>
+        <ConnectedSearchBarComponent />
+    </div>
+);
+HomeHeader.displayName = 'HomeHeader';
+export default HomeHeader;
