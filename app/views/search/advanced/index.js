ef047056335d4fe4f71719d7ad8e47cbbb8529a7
Thomas <tgallon@gmail.com>
2016-12-13 19:32:53 +0100

    advanced search

--- app/views/search/advanced/index.js
+++ app/views/search/advanced/index.js
@@ -3,6 +3,7 @@ import compose from 'lodash/flowRight';
 import ConnectedAdvancedSearch from '../../components/search/connected-advanced-search';
 import SearchHeader from '../../components/search/header';
 import {connect as connectToHeader} from 'focus-application/behaviours/header';
+import DemoTitle from '../../components/demo-title';
 
 
 class AdvancedSearchView extends PureComponent {
