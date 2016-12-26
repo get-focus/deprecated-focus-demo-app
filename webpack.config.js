b38d5d2aec2918ac7f40ad0b6982549fea17b628
Ephrame <viollet.ame@gmail.com>
2016-12-26 16:46:34 +0100

    [package] add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -26,11 +26,7 @@ const configuration = {
 const customConfig = localFocus ? {
     resolve: {
         alias: {
-            'focus-core': path.resolve(process.cwd(), '../focus-core'),
-            'focus-components': path.resolve(process.cwd(), '../focus-components'),
-            moment: path.resolve(process.cwd(), './node_modules/moment'),
-            numeral: path.resolve(process.cwd(), './node_modules/numeral'),
-            react: path.resolve(process.cwd(), './node_modules/react')
+            'focus-search': path.resolve(process.cwd(), '../../focus-search')
         }
     }
 } : {};
