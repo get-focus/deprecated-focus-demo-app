7df9bcb5941759621aaec66c2dffa25bcb88b2b3
GtAntoine <antoine.gt@orange.fr>
2017-03-02 10:36:58 +0100

    fetch with new WS

--- app/services/person.js
+++ app/services/person.js
@@ -8,22 +8,24 @@ const fetch = fetchBuilder({
 
 export default {
     loadPerson(id) {
-        console.log(`[PERSON] call loadPerson(${id}) method`);
         return fetch({url: `${id}`, method: 'GET'});
     },
+    loadPersonBiography(id) {
+        return fetch({url: `${id}/biography`, method: 'GET'});
+    },
+    loadPersonIdentity(id) {
+        return fetch({url: `${id}/identity`, method: 'GET'});
+    },
     loadPersonMovies(id) {
-        console.log(`[PERSON] call loadPersonMovies(${id}) method`);
         return fetch({url: `${id}`, method: 'GET'});
     },
     updatePersonBiography({person}) {
         const personId = person.code;
-        console.log(`[PERSON] call updatePersonBiography method. data=${JSON.stringify(person)}`);
         return fetch({url: `${personId}`, method: 'POST', data: omit(person, ['movieLinks'])});//, {isCORS: true});
     // return fetch(personUrl.update({urlData: {id: personId}, data: omit(person, ['movieLinks'])}), {isCORS: true});
     },
     updatePersonIdentity({person}) {
         const personId = person.code;
-        console.log(`[PERSON] call updatePersonIdentity method. data=${JSON.stringify(person)}`);
         return fetch({url: `${personId}`, method: 'POST', data: omit(person, ['movieLinks'])});
         // return fetch(personUrl.update({urlData: {id: personId}, data: omit(person, ['movieLinks'])}), {isCORS: true});
     }
